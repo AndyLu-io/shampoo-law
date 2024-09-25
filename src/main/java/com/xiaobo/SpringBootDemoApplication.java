e4b457711e3b076f19430c3cf8d9c2a6b9824bf0
luxiaobo <1135067601@qq.com>
2024-09-25 16:14:44 +0800

    update

--- src/main/java/com/xiaobo/SpringBootDemoApplication.java
+++ src/main/java/com/xiaobo/SpringBootDemoApplication.java
@@ -2,13 +2,8 @@ package com.xiaobo;
 
 import com.xiaobo.model.ConfigBean;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
