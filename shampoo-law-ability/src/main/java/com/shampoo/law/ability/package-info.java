739123512459834af183d713595ef973e70ebe79
luxiaobo <1135067601@qq.com>
2024-09-25 17:16:27 +0800

    project init

---
+++ shampoo-law-ability/src/main/java/com/shampoo/law/ability/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @Description
+ * @author luxiaobo
+ * @create 2024 -09 -25 -17:10
+ */
+package com.shampoo.law.ability;
---
+++ shampoo-law-extension/src/main/java/com/shampoo/law/extension/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @Description
+ * @author luxiaobo
+ * @create 2024 -09 -25 -17:14
+ */
+package com.shampoo.law.extension;
